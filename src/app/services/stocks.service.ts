af4e3babeff2961b0c3d86b88e6c303fff4803a1
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-27 20:39:44 +0100

    adding stocks service implementation

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+let stocks: Array <string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service: string = 'https://angular2-in-action-api.herokuapp.com';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class StocksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get() {
+    return stocks.slice();
+  }
+
+  add(stock: string) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  remove(stock: string) {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  load(symbols: Array<string>) {
+    if (symbols) {
+      return this.http.get<Array<StockInterface>>(service + '/stocks/snapshot?symbols=' + symbols.join());
+    }
+    return null;
+  }
+
 }
