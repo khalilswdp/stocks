9242f1154d32efda0c140e5e48c43c217d3f2f13
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-27 23:28:32 +0100

    Add the logic for managing the symbols/stocks

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StocksService} from "../../services/stocks.service";
 
 @Component({
   selector: 'app-manage',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() { }
+  symbols!: Array<string>;
+  stock!: string;
+
+  constructor(private service: StocksService) { }
 
   ngOnInit(): void {
+    this.symbols = this.service.get();
+  }
+
+  add() {
+    this.symbols = this.service.add(this.stock);
+    this.stock = '';
+  }
+
+  remove(symbol: string) {
+    this.symbols = this.service.remove(symbol);
   }
 
 }
